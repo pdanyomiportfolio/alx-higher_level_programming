fa1fea1a66518b6b615bc2c9aa5abdc9781911fd
Prince Anyomi <anyomiprince27@gmail.com>
2022-12-21 17:42:20 -0800

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
-""" Creating a square class """
+"""Creating a square class"""
 
 class Square:
-
-    """ Creating a square class """
-
+    """Creating a square class"""
     pass
