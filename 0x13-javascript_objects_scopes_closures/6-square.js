6e496912f1ada2a48d0c6a1fcbd43c04819750b9
Prince Anyomi <anyomiprince27@gmail.com>
2023-03-15 13:13:17 -0700

    corrected main

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,13 +1,21 @@
 #!/usr/bin/node
-
-module.exports = class Square extends require('./5-square') {
+const square = require('./5-square');
+class Square extends square {
+  /**
+   * @property {method} charPrint - prints the rectangle using the character c
+   * @returns void
+   */
   charPrint (c) {
     if (c === undefined) {
-      this.print();
-    } else {
-      for (let x = 0; x < this.height; x++) {
-        console.log(c.repeat(this.width));
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
       }
+      console.log(s);
     }
   }
-};
+}
+module.exports = Square;
